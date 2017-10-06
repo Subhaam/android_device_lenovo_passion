189c5f72e22fae2fb1aa28ebc0e653dbba13b526
Subhaam <derksoul98@gmail.com>
2017-10-06 11:12:57 +0530

    passion: AEX-fy
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- aosp_passion.mk
+++ aosp_passion.mk
@@ -15,16 +15,15 @@
 # Inherit device configuration
 $(call inherit-product, device/lenovo/passion/full_passion.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
+# Boot animation res
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := passion
-PRODUCT_NAME := lineage_passion
+PRODUCT_NAME := aosp_passion
 PRODUCT_BRAND := Lenovo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_RELEASE_NAME := passion
